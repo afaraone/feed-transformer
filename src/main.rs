d3d7633919ea96da9532db79478c8bbd19683bd0
Andres Faraone-Pirie <andres.faraone-pirie@songkick.com>
2026-01-18 14:29:16 +0000

    rename file

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use feed_transformer::stream::StreamIterator;
 const TOP_LEVEL_KEY: &str = "events";
 
 fn main() {
-    let stream_iterator = StreamIterator::new("./files/tm_US_events.json", TOP_LEVEL_KEY).unwrap();
+    let stream_iterator = StreamIterator::new("./files/events.json", TOP_LEVEL_KEY).unwrap();
     println!("counting events!");
 
     let music_events = stream_iterator.filter(|event| event.as_ref().is_ok_and(|e| e.is_music()));
