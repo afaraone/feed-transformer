52d4789957388d0f3b9db119f02df6b5a75b9522
Andres Faraone-Pirie <andres.faraone-pirie@songkick.com>
2026-01-20 17:31:07 +0000

    Implement custom errors for TmEvent transformation

--- src/tm_event.rs
+++ src/tm_event.rs
@@ -1,6 +1,7 @@
 use crate::sk_event::SkEvent;
 use chrono::{NaiveDate, NaiveTime};
 use serde::Deserialize;
+use thiserror::Error;
 
 #[allow(dead_code)]
 #[derive(Deserialize, Debug, Default)]
@@ -21,7 +22,7 @@ pub struct TmEvent {
 }
 
 #[allow(dead_code)]
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Default)]
 #[serde(rename_all = "camelCase")]
 struct Venue {
     venue_id: Option<String>,
@@ -34,13 +35,13 @@ struct Venue {
 }
 
 #[allow(dead_code)]
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Default)]
 struct AttractionWrapper {
     attraction: Attraction,
 }
 
 #[allow(dead_code)]
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Default)]
 #[serde(rename_all = "camelCase")]
 struct Attraction {
     attraction_id: Option<String>,
@@ -49,9 +50,17 @@ struct Attraction {
     classification_sub_type: Option<String>,
 }
 
+#[derive(Error, Debug)]
+pub enum TransformError {
+    #[error("Invalid event: {0}")]
+    InvalidEvent(String),
+
+    #[error("Ignored event")]
+    IgnoredEvent,
+}
+
 const TM_MUSIC_CLASSIFICATION_ID: &str = "KZFzniwnSyZfZ7v7nJ";
 
-#[allow(dead_code)]
 impl TmEvent {
     fn is_music_event(&self) -> bool {
         if let Some(classification_segment_id) = &self.classification_segment_id {
@@ -61,20 +70,94 @@ impl TmEvent {
         }
     }
 
-    pub fn try_into_sk_event(self) -> Result<SkEvent, String> {
+    pub fn try_into_sk_event(self) -> Result<SkEvent, TransformError> {
         if !self.is_music_event() {
-            return Err("Must be music event!".to_string());
+            return Err(TransformError::IgnoredEvent);
         }
 
-        let id = self.event_id.ok_or("Missing id!")?;
-        let title = self.event_name.ok_or("Missing title!")?;
-        let start_date = self.event_start_local_date.ok_or("Missing start_date!")?;
-        let start_time = self.event_start_local_time.ok_or("Missing start_time!")?;
+        let id = self
+            .event_id
+            .ok_or_else(|| TransformError::InvalidEvent("Missing id".into()))?;
+
+        let title = self
+            .event_name
+            .ok_or_else(|| TransformError::InvalidEvent("Missing title".into()))?;
+
+        let start_date = self
+            .event_start_local_date
+            .ok_or_else(|| TransformError::InvalidEvent("Missing start_date".into()))?;
+
+        let start_time = self
+            .event_start_local_time
+            .ok_or_else(|| TransformError::InvalidEvent("Missing start_time".into()))?;
+
         let venue_name = self
             .venue
             .and_then(|v| v.venue_name)
-            .ok_or("Missing venue name")?;
+            .ok_or_else(|| TransformError::InvalidEvent("Missing venue_name".into()))?;
 
         Ok(SkEvent::new(id, title, start_date, start_time, venue_name))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn valid_music_event() -> TmEvent {
+        TmEvent {
+            event_id: Some("event_id".to_string()),
+            event_start_local_date: Some(NaiveDate::default()),
+            event_name: Some("event title".to_string()),
+            event_start_local_time: Some(NaiveTime::default()),
+            classification_segment_id: Some(TM_MUSIC_CLASSIFICATION_ID.to_string()),
+            venue: Some(Venue {
+                venue_name: Some("venue name".to_string()),
+                ..Default::default()
+            }),
+            ..Default::default()
+        }
+    }
+
+    fn non_music_event() -> TmEvent {
+        TmEvent {
+            classification_segment_id: Some("foo".to_string()),
+            ..Default::default()
+        }
+    }
+
+    fn invalid_music_event_missing_event_name() -> TmEvent {
+        TmEvent {
+            event_id: Some("event_id".to_string()),
+            event_start_local_date: Some(NaiveDate::default()),
+            event_start_local_time: Some(NaiveTime::default()),
+            classification_segment_id: Some(TM_MUSIC_CLASSIFICATION_ID.to_string()),
+            venue: Some(Venue {
+                venue_name: Some("venue name".to_string()),
+                ..Default::default()
+            }),
+            ..Default::default()
+        }
+    }
+
+    #[test]
+    fn test_try_into_sk_event_with_valid_music_event() {
+        let tm_event = valid_music_event();
+        let result = tm_event.try_into_sk_event();
+        assert!(matches!(result, Ok(_)));
+    }
+
+    #[test]
+    fn test_try_into_sk_event_with_non_music_event() {
+        let tm_event = non_music_event();
+        let result = tm_event.try_into_sk_event();
+        assert!(matches!(result, Err(TransformError::IgnoredEvent)));
+    }
+
+    #[test]
+    fn test_try_into_sk_event_with_invalid_music_event() {
+        let tm_event = invalid_music_event_missing_event_name();
+        let result = tm_event.try_into_sk_event();
+        assert!(matches!(result, Err(TransformError::InvalidEvent(_))));
     }
 }
